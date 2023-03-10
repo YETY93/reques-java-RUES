7331432b2d360ef91bea68082c28f067f6af8c66
Yesid R <yety93@hotmail.com>
2023-03-09 23:35:08 -0500

    first commit

---
+++ src/main/java/com/yety/project/request/RequesjavaApplication.java
@@ -0,0 +1,13 @@
+package com.yety.project.request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequesjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequesjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yety/project/request/RequesjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yety.project.request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequesjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
