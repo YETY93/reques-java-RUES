08baa9d8da4f1517d71a11b0dedc8de0900cac69
Yesid R <yety93@hotmail.com>
2023-03-10 00:02:26 -0500

    Creacion de clases principales

---
+++ src/main/java/com/yety/project/request/comun/requestRUES.java
@@ -0,0 +1,5 @@
+package com.yety.project.request.comun;
+
+public class requestRUES {
+
+}
---
+++ src/main/java/com/yety/project/request/controller/requestRUEScontroller.java
@@ -0,0 +1,12 @@
+package com.yety.project.request.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rues/nit")
+public class requestRUEScontroller {
+	
+	
+
+}
